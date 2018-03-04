68d638c443a44c1cbdee191ebd636bbc03d08984
vmcoronel <36961896+vmcoronel@users.noreply.github.com>
2018-03-04 19:45:34 +0800

    Update msp430-binutils.rb

--- msp430-binutils.rb
+++ msp430-binutils.rb
@@ -4,7 +4,7 @@ class Msp430Binutils < Formula
   sha256 "12c26349fc7bb738f84b9826c61e103203187ca2d46f08b82e61e21fcbc6e3e6"
 
   patch do
-    url "https://downloads.sourceforge.net/project/mspgcc/Patches/binutils-2.22/msp430-binutils-2.22-20120911.patch"
+    url "https://sourceforge.net/p/mspgcc/mspgcc/ci/master/tree/msp430-binutils-2.22-20120911.patch"
     sha256 "1dc3cfb0eac093b5f016f4264b811b4352515e8a3519c91240c73bacd256a667"
   end
 
